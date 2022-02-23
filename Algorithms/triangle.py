cd34edc898e83ac0c1c75c56bc2de91d1ec128c7
EggerMarc <eggermarc@gmail.com>
2022-02-23 14:27:47 +0100

    test arrays commited in triangle

--- Algorithms/triangle.py
+++ Algorithms/triangle.py
@@ -8,7 +8,7 @@ def triangleInequality(M):
 
 if __name__ == '__main__':
     arr1 = [1, 0.1, 1.5, 3]
-    arr2 = [0.1, 1, 2, 0.2]
-    arr3 = [1.5, 2, 1, 0.6]
-    arr4 = [3, 0.2, 0.6, 1]
+    arr2 = [1/0.1, 1, 2, 0.2]
+    arr3 = [1/1.5, 1/2, 1, 0.6]
+    arr4 = [1/3, 1/0.2, 1/0.6, 1]
     triangleInequality()
